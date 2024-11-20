92d1f6a01b9dd461d66e4db03d21b1020ba4c58f
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2024-11-20 10:34:57 -0300

    feat : addCustomUserDetailsService class

---
+++ src/main/java/com/login/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.login.infra.security;
+
+import com.login.domain.model.User;
+import com.login.domain.repositories.UserRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
