1227742d56790870c1dda337100c8783e295d409
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2024-11-20 10:38:54 -0300

    feat : create UserRepository interface

--- src/main/java/com/login/domain/repositories/UserRepository.java
+++ src/main/java/com/login/domain/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.login.domain.repositories;
 import com.login.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
